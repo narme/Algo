f8126c68b9e398409fa0e0cb7fabedd1ab2ad217
narme <tjdtn5583@naver.com>
2023-05-13 01:36:52 +0900

    7579 solve

---
+++ 7579/7579.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+using namespace std;
+
+vector<int> memory, cost;
+int dp[10001];
+int n, m, cost_sum;
+
+void input(){
+    cin >> n >> m;
+    
+    int tmp;
+    for(int i = 0; i < n; i++){
+        cin >> tmp;
+        memory.push_back(tmp);
+    }
+    for(int i = 0; i < n; i++){
+        cin >> tmp;
+        cost_sum += tmp;
+        cost.push_back(tmp);
+    }
+}
+
+int solve(){
+    for(int i = 0; i < n; i++){
+        for(int j = cost_sum; j > 0; j--){
+            if(j - cost[i] >= 0)dp[j] = max(dp[j], dp[j-cost[i]] + memory[i]);
+        }
+    }
+    int minimum = 0;
+    while(dp[minimum] < m)minimum++;
+    return minimum;
+}
+
+int main(){
+    input();
+    cout << solve();
+}
